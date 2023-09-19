3930c7a0b1c2dc72260b3c29c0b66d0a4fa7aca4
Anto <japanto20@gmail.com>
2023-09-19 14:20:58 -0500

    Create servers

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,5 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+sudo yum install https://rpm.nodesource.com/pub_20.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+sudo yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
 dnf install nodejs -y
 useradd roboshop
 mkdir /app
