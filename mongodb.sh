e2f2377733be07b18f532d5a2d32c28637a82403
Anto <japanto20@gmail.com>
2023-09-20 15:29:36 -0500

    Create servers

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ source common.sh
 print_head "Setup MongoDB repository"
 # shellcheck disable=SC2154
 # shellcheck disable=SC1009
-cp "${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>"${log_file}"
+cp "${code_dir}"/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>"${log_file}"
 
 print_head "Install MongoDB"
 dnf install mongodb-org -y &>>"${log_file}"
@@ -15,9 +15,6 @@ print_head "Enable MongoDB"
 systemctl enable mongod &>>"${log_file}"
 
 print_head "Start MongoDB Service"
-# shellcheck disable=SC1073
-# shellcheck disable=SC1073
-# shellcheck disable=SC1073
 systemctl restart mongod &>>"${log_file}"
 
 # update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
