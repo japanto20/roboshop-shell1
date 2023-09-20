d507425ea26bcf67a15e46e6d27377d205cfae20
Anto <japanto20@gmail.com>
2023-09-20 15:08:43 -0500

    Create servers

--- mongodb.sh
+++ mongodb.sh
@@ -7,6 +7,9 @@ cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>"${log_file}"
 print_head "Install MongoDB"
 dnf install mongodb-org -y &>>"${log_file}"
 
+print_head "Update MongoDB listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+
 print_head "Enable MongoDB"
 systemctl enable mongod &>>"${log_file}"
 
