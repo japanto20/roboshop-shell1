f95373d65ae3387449bcb8322cd8e1caf31b6c20
Anto <japanto20@gmail.com>
2023-09-20 15:51:03 -0500

    Create servers

--- common.sh
+++ common.sh
@@ -5,3 +5,7 @@ rm -f ${log_file}
 print_head() {
   echo -e "\e[35m$1\e[0m"
 }
+
+print_status() {
+  echo $?
+}
--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,22 @@
 source common.sh
 
 print_head "Installing nginx"
-dnf install nginx -y &>>${log_file}
+dnf install nginx -y &>>${log_file
+echo $?
 
 print_head "Removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+print_status
 
 print_head "Downloading Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+print_status
 
 print_head "Extracting Downloaded Frontend"
 # shellcheck disable=SC2164
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
+print_status
 
 print_head "Copying nginx for roboshop"
 cp "${code_dir}"/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
