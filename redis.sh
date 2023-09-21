51b14fd149207c6be3be4a9c2352d8c7b45f9118
Anto <japanto20@gmail.com>
2023-09-21 09:43:36 -0500

    Create servers

---
+++ redis.sh
@@ -0,0 +1,25 @@
+source common.sh
+
+print_head "Installing Redis Repo files"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>"${log_file}"
+status_check $?
+
+print_head "Enable Redis 6.2 from package streams"
+dnf module enable redis:remi-6.2 -y &>>"${log_file}"
+status_check $?
+
+print_head "Install Redis"
+dnf install redis -y &>>"${log_file}"
+status_check $?
+
+print_head "Update redis listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>"${log_file}"
+status_check $?
+
+print_head "Enable redis"
+systemctl enable redis &>>"${log_file}"
+status_check $?
+
+print_head "start redis"
+systemctl start redis &>>"${log_file}"
+status_check $?
