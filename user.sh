c9f1814d9aa48db9a1a9a2eb18bff965a604d617
Anto <japanto20@gmail.com>
2023-09-21 09:56:36 -0500

    Create servers

--- user.sh
+++ user.sh
@@ -64,5 +64,5 @@ dnf install mongodb-org-shell -y &>>"${log_file}"
 status_check $?
 
 print_head "Load Schema"
-mongo --host mongodb.antodevops20.online </app/schema/catalogue.js &>>"${log_file}"
+mongo --host mongodb.antodevops20.online </app/schema/user.js &>>"${log_file}"
 status_check $?
