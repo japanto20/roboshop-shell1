aa9c90e26d8e5175ac18f2839f55bd0962465c7e
Anto <japanto20@gmail.com>
2023-09-20 20:28:07 -0500

    Create servers

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,11 @@ source common.sh
 
 print_head "Installing nginx"
 dnf install nginx -y &>>${log_file}
-Exit_status
+if [ $? -eq 0]; then
+  echo success
+else
+  echo failure
+fi
 
 print_head "Removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
