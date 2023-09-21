9f3467c1cb9efe9edaf905c35068032695bfc400
Anto <japanto20@gmail.com>
2023-09-20 21:18:28 -0500

    Create servers

--- catalogue.sh
+++ catalogue.sh
@@ -18,7 +18,9 @@ fi
 status_check $?
 
 print_head "Create app directory"
-mkdir /app &>>"${log_file}"
+if [ ! -d /app ]; then
+  mkdir /app &>>"${log_file}"
+fi
 status_check $?
 
 print_head "Delete old content"
