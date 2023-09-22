20885bf3c8888f5e55e099490e4c0dcd998da93f
Anto <japanto20@gmail.com>
2023-09-21 21:17:12 -0500

    Create servers

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,7 @@ source common.sh
 
 mysql_root_password=$1
 if [ -z "${mysql_root_password}" ]; then
-  echo -e "\e31mMissing Mysql Root Password argument\e[om"
+  echo -e "\e[31mMissing Mysql Root Password argument\e[om"
   exit 1
 fi
 
