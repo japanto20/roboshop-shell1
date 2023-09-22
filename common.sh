3f3e80af5ba94607a375c6cbe917b33b5bb8953d
Anto <japanto20@gmail.com>
2023-09-22 17:08:15 -0500

    Create servers

--- common.sh
+++ common.sh
@@ -30,9 +30,66 @@ print_head "Load Schema"
 mongo --host mongodb.antodevops20.online </app/schema/"${component}".js &>>"${log_file}"
 status_check $?
 
+elif [ "${schema_type}" == "mysql" ];
+  print_head "install mysql"
+  status_check $?
+  dnf install mysql -y &>>"${log_file}"
+  print_head "Load Schema"
+  mysql -h mysql.antodevops20.online -uroot -p${mysql_root_password}  < /app/schema/shipping.sql &>>"${log_file}"
+  status_check $?
 fi
+
 }
 
+app_prereq_setup() {
+
+  print_head "Create Roboshop user"
+  id roboshop &>>"${log_file}"
+  if [ $? -ne 0 ]; then
+   useradd roboshop &>>"${log_file}"
+  fi
+  status_check $?
+
+  print_head "Create app directory"
+  if [ ! -d /app ]; then
+    mkdir /app &>>"${log_file}"
+  fi
+  status_check $?
+
+  print_head "Delete old content"
+  rm -rf /app/* &>>"${log_file}"
+  status_check $?
+
+  print_head "Downloading app content"
+  curl -o /tmp/"${component}".zip https://roboshop-artifacts.s3.amazonaws.com/"${component}".zip &>>"${log_file}"
+  status_check $?
+  cd /app
+
+  print_head "Extracting app content"
+  unzip /tmp/"${component}".zip &>>"${log_file}"
+  status_check $?
+}
+
+systemd_setup() {
+
+  print_head "Copy systemD service file"
+  # shellcheck disable=SC2154
+  cp "${code_dir}"/configs/"${component}".service /etc/systemd/system/"${component}".service &>>"${log_file}"
+  status_check $?
+
+  print_head "Reload systemD"
+  systemctl daemon-reload &>>"${log_file}"
+  status_check $?
+
+  print_head "Enable ${component} service"
+  systemctl enable "${component}" &>>"${log_file}"
+  status_check $?
+
+  print_head "Start ${component} service"
+  systemctl restart "${component}" &>>"${log_file}"
+  status_check $?
+
+}
 nodejs() {
 print_head "Configure NodeJS repo"
 sudo yum install https://rpm.nodesource.com/pub_20.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y  &>>"${log_file}"
@@ -44,53 +101,35 @@ print_head "Install NodeJS"
 dnf install nodejs -y &>>"${log_file}"
 status_check $?
 
-print_head "Create Roboshop user"
-id roboshop &>>"${log_file}"
-if [ $? -ne 0 ]; then
- useradd roboshop &>>"${log_file}"
-fi
-status_check $?
-
-print_head "Create app directory"
-if [ ! -d /app ]; then
-  mkdir /app &>>"${log_file}"
-fi
-status_check $?
-
-print_head "Delete old content"
-rm -rf /app/* &>>"${log_file}"
-status_check $?
-
-print_head "Downloading app content"
-curl -o /tmp/"${component}".zip https://roboshop-artifacts.s3.amazonaws.com/"${component}".zip &>>"${log_file}"
-status_check $?
-cd /app
-
-print_head "Extracting app content"
-unzip /tmp/"${component}".zip &>>"${log_file}"
-status_check $?
+app_prereq_setup
 
 print_head "IInstall Nodejs dependencies"
 npm install &>>"${log_file}"
 status_check $?
 
-print_head "Copy systemD service file"
-# shellcheck disable=SC2154
-cp "${code_dir}"/configs/"${component}".service /etc/systemd/system/"${component}".service &>>"${log_file}"
-status_check $?
-
-print_head "Reload systemD"
-systemctl daemon-reload &>>"${log_file}"
-status_check $?
-
-print_head "Enable ${component} service"
-systemctl enable "${component}" &>>"${log_file}"
-status_check $?
-
-print_head "Start ${component} service"
-systemctl start "${component}" &>>"${log_file}"
-status_check $?
 
 schema_setup
+
+systemd_setup
+
+}
+
+java() {
+
+  print_head "Install maven"
+  dnf install maven -y &>>"${log_file}"
+  status_check $?
+
+  app_prereq_setup
+
+  print_head "download the dependencies & build the application."
+  cd /app &>>"${log_file}"
+  mvn clean package &>>"${log_file}"
+  mv target/"${component}"-1.0.jar "${component}".jar &>>"${log_file}"
+  status_check $?
+
+  schema_setup
+
+  systemd_setup
 
 }
