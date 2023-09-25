208920a54bc4d753e7616f4edfbc259c9f62a23c
Anto <japanto20@gmail.com>
2023-09-25 18:30:24 -0500

    Create servers

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,7 +2,7 @@ source common.sh
 
 roboshop_app_password=$1
 if [ -z "${mysql_root_password}" ]; then
-  echo -e "\e[31mMissing Mysql Root Password argument\e[0m"
+  echo -e "\e[31mMissing rabbitmq app user Password argument\e[0m"
   exit 1
 fi
 
