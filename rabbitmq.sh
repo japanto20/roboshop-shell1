29cdd77b88da699cd951ba21bbdb0f7727624392
Anto <japanto20@gmail.com>
2023-09-25 18:27:08 -0500

    Create servers

---
+++ rabbitmq.sh
@@ -0,0 +1,35 @@
+source common.sh
+
+roboshop_app_password=$1
+if [ -z "${mysql_root_password}" ]; then
+  echo -e "\e[31mMissing Mysql Root Password argument\e[0m"
+  exit 1
+fi
+
+print_head "Setup Erland repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>"${log_file}"
+status_check $?
+
+print_head "setup rabbitmq repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>"${log_file}"
+status_check $?
+
+print_head "Install rabbitmq server
+dnf install rabbitmq-server -y &>>"${log_file}"
+status_check $?
+
+print_head "Enable rabbitmq server"
+systemctl enable rabbitmq-server &>>"${log_file}"
+status_check $?
+
+print_head "start rabbitmq server"
+systemctl start rabbitmq-server &>>"${log_file}"
+status_check $?
+
+print_head "add user"
+rabbitmqctl add_user  ${roboshop_app_password} &>>"${log_file}"
+status_check $?
+
+print_head "set permission"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>"${log_file}"
+status_check $?
